88acd4e28357d290201a9b6077bc2f63402e3c5d
Adam Goth <adamgoth@gmail.com>
2016-08-21 10:25:12 -0600

    Adds custom view class for rounded corners on review table cells

---
+++ camp-colorado/CustomView.swift
@@ -0,0 +1,17 @@
+//
+//  CustomView.swift
+//  camp-colorado
+//
+//  Created by Adam Goth on 8/21/16.
+//  Copyright © 2016 Adam Goth. All rights reserved.
+//
+
+import UIKit
+
+class CustomView: UIView {
+    
+    override func awakeFromNib() {
+        layer.cornerRadius = 2.0
+    }
+    
+}
