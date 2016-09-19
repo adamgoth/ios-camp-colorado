6b54d18376c6f6b587076d962684f7df614973ab
Adam Goth <adamgoth@gmail.com>
2016-09-19 16:25:35 -0600

    Adds custom button class, adds class to login view buttons, cleans up UI frame conflicts on login view

---
+++ camp-colorado/CustomButton.swift
@@ -0,0 +1,17 @@
+//
+//  CustomButton.swift
+//  camp-colorado
+//
+//  Created by Adam Goth on 9/19/16.
+//  Copyright © 2016 Adam Goth. All rights reserved.
+//
+
+import UIKit
+
+class CustomButton: UIButton {
+    
+    override func awakeFromNib() {
+        layer.cornerRadius = 5.0
+    }
+    
+}
